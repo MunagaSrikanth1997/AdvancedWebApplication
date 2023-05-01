43b71a9f6865611ee7b5e12ff210f361e1051a28
Srikanth Munaga <srikanthsiri.ms@gmail.com>
2023-05-01 18:28:37 -0500

    implemented graph

--- client/processor.js
+++ client/processor.js
@@ -1,51 +1,90 @@
 'use strict'
+
+
 var data = (id) => document.getElementById(id);
 
-const getTransactionHistory=async ()=>{
-console.log("inside getTransactionHistory.....");
+var renderInvestmentPortfolio = async () => {
+    console.log("investmentportfolio...................................................................");
+    var history = new Map();
+    var labelList = [];
+    var dataSet = [];
+    var portfolioData = [];
+    await fetch("http://localhost:8080/getInvestmentportfolio",
+        {
+            method: "GET"
+        }).then(response => response.json()
+        ).then(data => {
+            console.log(data);
+            const ctx = document.getElementById('acquisitions');
 
-await fetch("http://localhost:8080/getTransactionsHistory",
-{
-    method:"GET"
-}).then((response)=>{
-    console.log("history process completed");
-    console.log(response);
-return response.json();
+            new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: data[0],
+                    datasets: [{
+                        label: 'amount spent by year',
+                        data: data[1],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
 
-}).then((result)=>{
-    console.log("************srikanthhhhhhh********");
-var history=new Map();
-    result.forEach(element=>{
-        if(history.get(element.TransactionDate)==null){
-            history.set(element.TransactionDate,[]);
-            history.get(element.TransactionDate).push(element);
-           
-element.TransactionDate= new Date(Date.UTC(0, 0, element.TransactionDate - 1));
-        }else{
-            history.get(element.TransactionDate).push(element);
-            element.TransactionDate= new Date(Date.UTC(0, 0, element.TransactionDate - 1));         
-        }
-    })
-   console.log(history);
-      history.forEach( (value, key, map) => {
-        var trans=document.createElement("div");
-        trans.id=key;
-        var bold=document.createElement("b");
-        var header=document.createElement("h3");
-        header.innerHTML="<b>" + new Date(Date.UTC(0, 0, key - 1)) +"</b>";
-        
-        trans.appendChild(header);
-        value.forEach(transaction=>{
-var para=document.createElement("p");
-para.innerHTML=transaction.TransactionName + "<br>" + transaction.AccountNumber +"<br>";
-trans.appendChild(para);
         });
-        data("history").appendChild(trans);
-      });
+
+}
+
+
+const getTransactionHistory = async () => {
+    console.log("inside getTransactionHistory.....");
+
+    await fetch("http://localhost:8080/getTransactionsHistory",
+        {
+            method: "GET"
+        }).then((response) => {
+            console.log("history process completed");
+            console.log(response);
+            return response.json();
+
+        }).then((result) => {
+            var history = new Map();
+            result.forEach(element => {
+                if (history.get(element.TransactionDate) == null) {
+                    history.set(element.TransactionDate, []);
+                    history.get(element.TransactionDate).push(element);
+
+                    element.TransactionDate = new Date(Date.UTC(0, 0, element.TransactionDate - 1));
+                } else {
+                    history.get(element.TransactionDate).push(element);
+                    element.TransactionDate = new Date(Date.UTC(0, 0, element.TransactionDate - 1));
+                }
+            })
+            console.log(history);
+            history.forEach((value, key, map) => {
+                var trans = document.createElement("div");
+                trans.id = key;
+                var bold = document.createElement("b");
+                var header = document.createElement("h3");
+                header.innerHTML = "<b>" + new Date(Date.UTC(0, 0, key - 1)) + "</b>";
+
+                trans.appendChild(header);
+                value.forEach(transaction => {
+                    var para = document.createElement("p");
+                    para.innerHTML = transaction.TransactionName + "<br>" + transaction.AccountNumber + "<br>" +"Amount  $ "+ transaction.Amount + "<br>";
+                    trans.appendChild(para);
+                });
+                data("history").appendChild(trans);
+            });
 
 
 
-});
+        });
 
 }
 
@@ -67,7 +106,7 @@ const excelProcessor = async () => {
             var td4 = document.createElement("td");
             td4.innerText = record.AccountType;
 
-            tableRow.append(td1, td2, td3,td4);
+            tableRow.append(td1, td2, td3, td4);
 
             document.getElementById("accountsBody").append(tableRow);
 
@@ -77,81 +116,80 @@ const excelProcessor = async () => {
 
 }
 
-const paymentsInfo=async ()=>{
-console.log("Payments Info");
+const paymentsInfo = async () => {
+    console.log("Payments Info");
 
-await fetch("http://localhost:8080/getPaymentsInfo", {
-    method: "GET"
+    await fetch("http://localhost:8080/getPaymentsInfo", {
+        method: "GET"
 
-}).then((res) => {
-    return res.json();
-}).then((respo)=>{
-console.log(respo);
+    }).then((res) => {
+        return res.json();
+    }).then((respo) => {
+        console.log(respo);
 
-var fromAccountsEligibleList = document.createElement("select");
-var toAccountsEligibleList = document.createElement("select");
-fromAccountsEligibleList.name = "fromAccountsList";
-fromAccountsEligibleList.id = "fromAccounts";
-toAccountsEligibleList.name="toAccountsList";
-toAccountsEligibleList.id="toAccounts";
+        var fromAccountsEligibleList = document.createElement("select");
+        var toAccountsEligibleList = document.createElement("select");
+        fromAccountsEligibleList.name = "fromAccountsList";
+        fromAccountsEligibleList.id = "fromAccounts";
+        toAccountsEligibleList.name = "toAccountsList";
+        toAccountsEligibleList.id = "toAccounts";
 
-var fromAccountLabel = document.createElement("label");
-fromAccountLabel.innerHTML = "From Account: "
-//fromAccountLabel.htmlFor = "pets";
+        var fromAccountLabel = document.createElement("label");
+        fromAccountLabel.innerHTML = "From Account: ";
 
-var toAccountLabel = document.createElement("label");
-toAccountLabel.innerHTML = "To Account: "
-//toAccountLabel.htmlFor = "pets";
+        var toAccountLabel = document.createElement("label");
+        toAccountLabel.innerHTML = "To Account: ";
 
 
-    respo.forEach(record=>{
-        if(record.isFromAccountTransferEligible){
-            var option = document.createElement("option");
-            option.value = record.AccountName;
-            option.text = record.AccountName;
-            fromAccountsEligibleList.appendChild(option);
-        }else if(record.isToAccountTransferEligible){
-            var option = document.createElement("option");
-            option.value = record.AccountName;
-            option.text = record.AccountName;
-            toAccountsEligibleList.appendChild(option);
-        }
-       
+        respo.forEach(record => {
+            if (record.isFromAccountTransferEligible) {
+                var option = document.createElement("option");
+                option.value = record.AccountName;
+                option.text = record.AccountName;
+                fromAccountsEligibleList.appendChild(option);
+            } else if (record.isToAccountTransferEligible) {
+                var option = document.createElement("option");
+                option.value = record.AccountName;
+                option.text = record.AccountName;
+                toAccountsEligibleList.appendChild(option);
+            }
+
+        });
+        var amount = document.createElement("label");
+        amount.innerHTML = "Amount: $"
+        data("paymentsInfo").append(fromAccountLabel);
+        data("paymentsInfo").append(fromAccountsEligibleList);
+        data("paymentsInfo").append(document.createElement("br"));
+        data("paymentsInfo").append(toAccountLabel);
+        data("paymentsInfo").append(toAccountsEligibleList);
+        data("paymentsInfo").append(document.createElement("br"));
+        data("paymentsInfo").append(amount);
+
+        var input = document.createElement("input");
+        input.type = "text";
+        input.id = "amount";
+        data("paymentsInfo").append(input);
+        data("paymentsInfo").append(document.createElement("br"));
+        var makePayment = document.createElement("input");
+        makePayment.type = "button";
+        makePayment.id = "makePayment";
+        makePayment.name = "make payment";
+        makePayment.value = "make payment";
+        makePayment.onclick = processPayment;
+        data("paymentsInfo").append(makePayment);
+
     });
-    var amount = document.createElement("label");
-amount.innerHTML = "Amount: $"
-    data("paymentsInfo").append(fromAccountLabel);
-data("paymentsInfo").append(fromAccountsEligibleList);
-data("paymentsInfo").append(document.createElement("br"));
-data("paymentsInfo").append(toAccountLabel);
-data("paymentsInfo").append(toAccountsEligibleList);
-data("paymentsInfo").append(document.createElement("br"));
-data("paymentsInfo").append(amount);
-
-var input=document.createElement("input");
-input.type="text";
-input.id="amount";
-data("paymentsInfo").append(input);
-data("paymentsInfo").append(document.createElement("br"));
-var makePayment=document.createElement("input");
-makePayment.type="button";
-makePayment.id="makePayment";
-makePayment.name="make payment";
-makePayment.value="make payment";
-makePayment.onclick=processPayment;
-data("paymentsInfo").append(makePayment);
-
-});
 }
 
-const processPayment=()=>{
-window.alert("payment is successfully processed");
+const processPayment = () => {
+    window.alert("payment is successfully processed");
 }
 
 $(document).ready(() => {
     excelProcessor();
     paymentsInfo();
     getTransactionHistory();
+    renderInvestmentPortfolio();
 });
 
 
@@ -160,29 +198,29 @@ var login = async () => {
     var userName = data("userName").value;
     var pwd = data("pwd").value;
     console.log(userName);
-    var params={
-        
+    var params = {
+
         userName: userName,
         password: pwd
-    
-}
 
-var url=new URL('http://localhost:8080/login');
-var postBody=new FormData();
-postBody.append("userName",userName);
-postBody.append("password",pwd);
- //url.searchParams.append("userName",userName);
- //url.searchParams.append("password",pwd);
+    }
+
+    var url = new URL('http://localhost:8080/login');
+    var postBody = new FormData();
+    postBody.append("userName", userName);
+    postBody.append("password", pwd);
+    //url.searchParams.append("userName",userName);
+    //url.searchParams.append("password",pwd);
     //console.log(params);
-    
+
     await fetch(url, {
-       
+
         method: "POST",
         headers: {
             'Content-Type': 'application/json'
-            },
-         body: JSON.stringify(params)
-        
+        },
+        body: JSON.stringify(params)
+
     }).then((response) => {
         return response.body;
     }).then((response) => {
--- server/app.js
+++ server/app.js
@@ -1,121 +1,138 @@
-// console.log(workbook.SheetNames);
-    // console.log(workbook.Sheets);
-    // console.log(workbook.Sheets['Niha']);
-    // console.log(xlsx.utils.sheet_to_json(workbook.Sheets['Niha']));
-
+'use strict'
 const express = require('express');
-const xlsx=require('xlsx');
+const xlsx = require('xlsx');
 const app = express();
 const cors = require('cors');
-var dataformatter=require('dateformatter');
+
+var dataformatter = require('dateformatter');
 app.use(cors());
 app.use(express.json());
 var bodyParser = require('body-parser');
 const dateformatter = require('dateformatter');
 app.use(bodyParser.urlencoded({ extended: false }));
-var workbook=xlsx.readFile('../DataRepo/Confidential.xlsx');
-const data = [{
-    "id": "1",
-    "accountName": "Checking",
-    "accountNumber":"0000125478963",
-    "accountType": "CHECKING",
-    "AvailBalance": "$500.35"
+var workbook = xlsx.readFile('../DataRepo/Confidential.xlsx');
 
-},
-{
-    "id": "2",
-    "accountName": "Checking123",
-    "accountNumber":"0000125478963",
-    "accountType": "CHECKING",
-    "AvailBalance": "$550.35"
-
-}];
-
-app.get('/getPaymentsInfo',(req,res)=>{
+app.get('/getPaymentsInfo', (req, res) => {
     console.log("getPaymentsInfo");
-const accountsInfo=[];
-    const eligibleFromAcct=[];
-    const eligibleToAcct=[];
+    const accountsInfo = [];
     xlsx.utils.sheet_to_json(workbook.Sheets['AccountsInfo']).forEach(element => {
-        req.query.Id=102548;
-        if(element.UserId===Number(req.query.Id)){
-           
-          if(element.isTransferEligible){
-if(element.isFromAccountTransferEligible){
-    accountsInfo.push(element);
-}else if(element.isToAccountTransferEligible){
-    accountsInfo.push(element);
-}
-          }
-        }else{
+        req.query.Id = 102548;
+        if (element.UserId === Number(req.query.Id)) {
+
+            if (element.isTransferEligible) {
+                if (element.isFromAccountTransferEligible) {
+                    accountsInfo.push(element);
+                } else if (element.isToAccountTransferEligible) {
+                    accountsInfo.push(element);
+                }
+            }
+        } else {
 
         }
-            
-        
-        
+
+
+
     });
     res.send(accountsInfo);
 });
 
-app.get('/getTransactionsHistory',(req,res)=>{
-console.log("inside getTransactionsHistory method");
+app.get('/getInvestmentportfolio', (req, res) => {
+
+    console.log("inside getinvestmentportfolio");
+    let history = [];
+    var labelList = [];
+    var dataSet = [];
+    var portfolioData = [];
+    var sortedData = new Map();
+    xlsx.utils.sheet_to_json(workbook.Sheets['TransactionsHistory']).forEach(element => {
+        req.query.Id = 102548;
+        if (element.UserId === Number(req.query.Id)) {
+            history.push(element)
+        } else {
+
+        }
 
 
-let history=[];
-xlsx.utils.sheet_to_json(workbook.Sheets['TransactionsHistory']).forEach(element => {
-    req.query.Id=102548;
-    if(element.UserId===Number(req.query.Id)){
-        history.push(element)
-    }else{
 
-    }
-        
-    
-    
+    });
+
+    history.forEach(element => {
+
+        if (sortedData.get(new Date(Date.UTC(0, 0, element.TransactionDate - 1)).getFullYear()) == null) {
+            sortedData.set(new Date(Date.UTC(0, 0, element.TransactionDate - 1)).getFullYear(), element.Amount);
+        } else {
+            sortedData.set(new Date(Date.UTC(0, 0, element.TransactionDate - 1)).getFullYear(), sortedData.get(new Date(Date.UTC(0, 0, element.TransactionDate - 1)).getFullYear()) + element.Amount)
+
+        }
+    });
+
+    sortedData.forEach((value, key, map) => {
+        labelList.push(key);
+        dataSet.push(value);
+    });
+
+    portfolioData.push(labelList);
+    portfolioData.push(dataSet);
+
+    res.send(portfolioData);
+
+
 });
 
-res.send(history);
+app.get('/getTransactionsHistory', (req, res) => {
+    console.log("inside getTransactionsHistory method");
+
+
+    let history = [];
+    xlsx.utils.sheet_to_json(workbook.Sheets['TransactionsHistory']).forEach(element => {
+        req.query.Id = 102548;
+        if (element.UserId === Number(req.query.Id)) {
+            history.push(element)
+        } else {
+
+        }
+
+
+
+    });
+
+    res.send(history);
 });
 
 
 app.get('/test', (req, res) => {
-    var accountsList=[];
+    var accountsList = [];
     xlsx.utils.sheet_to_json(workbook.Sheets['Accounts']).forEach(element => {
-        req.query.Id=102548;
-        if(element.Id===Number(req.query.Id)){
-           console.log(req.query.Id);
+        req.query.Id = 102548;
+        if (element.Id === Number(req.query.Id)) {
+            console.log(req.query.Id);
             accountsList.push(element)
-        }else{
+        } else {
 
         }
-            
-        
-        
+
+
+
     });
     res.send(accountsList);
 });
 
-app.post('/login',(req,res)=>{
-var resp;
-xlsx.utils.sheet_to_json(workbook.Sheets['UserInfo']).forEach(element => {
+app.post('/login', (req, res) => {
+    var resp;
+    xlsx.utils.sheet_to_json(workbook.Sheets['UserInfo']).forEach(element => {
 
-    if(element.userName==req.body.userName && element.password==req.body.password){
-       console.log(req.body.userName);
-       resp="success";
-    }else{
-resp="failure";
-    }
-        
-    
-    
-});
-// if(req.body.userName=="Srikanth"){
-// console.log("success");
-// }else{
-// console.log("failure");
-// }
-res.redirect('/test');
-res.send();
+        if (element.userName == req.body.userName && element.password == req.body.password) {
+            console.log(req.body.userName);
+            resp = "success";
+        } else {
+            resp = "failure";
+        }
+
+
+
+    });
+    res.redirect('/test');
+    res.send();
 });
 
 
